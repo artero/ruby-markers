edc0523dcf1634af45187039246721d867d96da4
Juan Artero <juan.artero@gmail.com>
2017-05-30 02:23:18 +0200

    little refactor

--- src/extension.ts
+++ src/extension.ts
@@ -19,13 +19,13 @@ export function activate(context: vscode.ExtensionContext) {
     marker.evaluate();
   });
 
-  let add_mark = vscode.commands.registerCommand('extension.add_mark', () => {
-    marker.add_mark();
+  let addMark = vscode.commands.registerCommand('extension.addMark', () => {
+    marker.addMark();
   });
 
   context.subscriptions.push(hello);
   context.subscriptions.push(evaluate);
-  context.subscriptions.push(add_mark);
+  context.subscriptions.push(addMark);
 }
 
 // this method is called when your extension is deactivated
@@ -39,7 +39,6 @@ class Markers {
     // Display a message box to the user
     const editor = vscode.window.activeTextEditor;
     const document = editor.document;
-    document.save;
     let content = document.getText().replace(/"/g, "\\\"");
     let comand = 'echo "' + content + '" | xmpfilter';
 
@@ -57,17 +56,19 @@ class Markers {
     });
   }
 
-  public add_mark(){
+  public addMark(){
     console.log('add mark');
     const editor = vscode.window.activeTextEditor;
     const document = editor.document;
     editor.edit((editBuilder) => {
       for (let selection of editor.selections) {
         let line = selection.end.line
+        var lineText = document.lineAt(line).text
         var pos1 = new vscode.Position(line, 0);
-        var pos2 = new vscode.Position(line, document.lineAt(line).text.length);
+        var pos2 = new vscode.Position(line, lineText.length);
         var range = new vscode.Range(pos1, pos2);
-        var output = document.lineAt(line).text + " # =>"
+        var spaces = (lineText.length < 50) ? 50 - lineText.length : 1
+        var output = lineText + " ".repeat(spaces) + "# =>"
         editBuilder.replace(range, output);
       }
     });
